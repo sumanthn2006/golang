de5a178e7de8db11d8cd6d69284db7f520dbfcf2
sumanthn2006 <tnsumanth91@gmail.com>
2025-09-21 17:04:41 +0530

    Resolved merge conflicts

--- day1/main.go
+++ day1/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello word")
+	fmt.Println("------------------VARIABLE-------------------------------")
+
+	var age int = 25 // integer
+	var name string = "Sumanth N"
+
+	fmt.Println("NAME:", name)
+	fmt.Println("AGE:", age)
+
+	var salary float64 = 50000.00
+	var active bool = true
+	fmt.Println("SALARY:", salary)
+	fmt.Println("ACTIVE:", active)
 }
