92614ce95d91582c5c6d908065006583fee5f785
sumanthn2006 <sn6121401@gmail.com>
2026-08-22 00:38:08 +0530

    leetcode

--- leetcode/main.go
+++ leetcode/main.go
@@ -14,8 +14,24 @@ func plusOne(digits []int) []int {
 	return append([]int{1}, digits...)
 
 }
+func Reverse() {
+	arr := [5]int{10, 20, 30, 40, 50}
+
+	//for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+	//	arr[i], arr[j] = arr[j], arr[i]
+	//}
+	alen := len(arr)
+	for i := 0; i < alen; i++ {
+		for j := i + 1; j < alen; j++ {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	fmt.Println(arr)
+}
 func main() {
 	result := plusOne([]int{1, 2, 3})
 	fmt.Println(result)
+	Reverse()
 
 }
