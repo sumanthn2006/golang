4d0e08507fa05c7a94eba0647bde44703c06558d
sumanthn2006 <sn6121401@gmail.com>
2026-08-21 12:41:46 +0530

    day5

--- day5/main.go
+++ day5/main.go
@@ -1,9 +1,27 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	n := 20
-	for i := 0; i <= n; i++ {
-		println(i)
+	//	n := 20
+	//	for i := 0; i <= n; i++ {
+	//		println(i)
+	//	}
+	x := 505
+	o := x
+	r := 0
+	for x > 0 {
+		d := x % 10
+		r = r*10 + d
+		x = x / 10
+
+	}
+	if r == o {
+		fmt.Println(r, "palindrome")
+	} else {
+		fmt.Println("not palindrome")
 	}
 
 }
