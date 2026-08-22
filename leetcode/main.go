495983857faf5e0b2b122af03e933cd7ea57aa55
sumanthn2006 <sn6121401@gmail.com>
2026-08-22 20:28:01 +0530

    leetcode

--- leetcode/main.go
+++ leetcode/main.go
@@ -45,6 +45,54 @@ func removeElement() {
 	fmt.Println(k)
 
 }
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	return build(nums, 0, len(nums)-1)
+}
+
+func build(nums []int, left int, right int) *TreeNode {
+
+	if left > right {
+		return nil
+	}
+
+	mid := left + (right-left)/2
+
+	root := &TreeNode{
+		Val: nums[mid],
+	}
+
+	root.Left = build(nums, left, mid-1)
+	root.Right = build(nums, mid+1, right)
+
+	return root
+}
+func maxProfit(prices []int) int {
+
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for i := 1; i < len(prices); i++ {
+
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+
+		profit := prices[i] - minPrice
+
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+
+	return maxProfit
+}
 func main() {
 	result := plusOne([]int{1, 2, 3})
 	fmt.Println(result)
